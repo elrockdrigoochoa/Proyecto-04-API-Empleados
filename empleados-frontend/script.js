e5edab59c921c26229f514c1df339d80fd392fa6
“Rodrigo <rodrigo.element2010@gmail.com>
2024-07-22 18:23:11 -0400

    Primer avance

---
+++ empleados-frontend/script.js
@@ -0,0 +1,90 @@
+const registerFormElement = document.querySelector('#empleadoForm');
+const searchFormElement = document.querySelector('#searchForm');
+
+const url = 'http://localhost:3000/empleados';
+
+const getEmpleados = async (nameSearched) => {
+
+    const newUrl = !nameSearched ? url : `${url}?name=${nameSearched}`;
+
+    try {
+        const response = await fetch(newUrl);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const empleados = await response.json();
+        return empleados;
+
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+const createEmpleado = async (empleado) => {
+    try {
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(empleado),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        } else {
+            getData();
+        }
+
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+const generateEmpleadosView = (empleadosArray) => {
+
+    const ulElement = document.querySelector('ul');
+    ulElement.innerHTML = '';
+
+    //(empleadosArray[i], i)
+    empleadosArray.forEach((element, index) => {
+        // console.log(`empleado[${index}]: ${element.name} - ${element.apellido}`); // empleado [0]:
+        const liElement = document.createElement('li');
+        liElement.innerText = `${element.name} - ${element.apellido}`;
+        ulElement.append(liElement);
+
+    });
+
+};
+
+registerFormElement.addEventListener('submit', (event) => {
+    // console.log(event); // todas las propiedas de la web api que tiene ese evento
+    // console.log(event.target); // el elemento html que lanza el evento
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const song = formData.get('empleadoName');
+    const artist = formData.get('apellido');
+    const link = formData.get('curriculumLink');
+
+    const body = { name: empleado, apellido: apellido, curriculum: link };
+    createEmpleado(body);
+
+});
+
+searchFormElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nameSearched = document.querySelector('#nameSearched').value;
+    getData(nameSearched);
+});
+
+const getData = async (nameSearched) => {
+    const empleados = await getEmpleados(nameSearched);
+    generateSongsView(empleados);
+};
+
+getData();
---
+++ express/empleados-api/src/index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('servidor ok!');
+});
+
+app.get('/hellow', (req, res) => {
+    res.send('<h1> Hola desde express! </h1>');
+});
+
+const songs = [
+    { id: 1, name: 'Juan', apellido: 'Perez', curriculum: '' },
+    { id: 2, name: 'Maria', apellido: 'Ramirez', curriculum: '' },
+    { id: 3, name: 'Raul', apellido: 'Zaragoza', curriculum: '' },
+    { id: 4, name: 'Javier', apellido: 'Rojas', curriculum: '' },
+    { id: 4, name: 'Valeria', apellido: 'Ochoa', curriculum: '' },
+    { id: 6, name: 'Mauricio', apellido: 'Rangel', curriculum: '' },
+];
+
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500',
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// GET: Obtener todos los empleados
+app.get('/empleados', (req, res) => {
+
+    const name = req.query.name;
+
+    if (!name) {
+        res.json(empleados);
+    }
+
+    const filteredEmpleadosByName = songs.filter((empleados) => {
+        return empleados.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())
+    });
+
+    res.json(filteredEmpleadossByName);
+});
+
+// GET: Obtener un empleado en especifico
+// request params: parametros que se mandan en la url y se utilizan para selecionar recursos. Enmedio de la url. Ejemplo: api/pokemon/125/algo
+// quey params: son parametros que se utilizar como parametro para filtrar informacion. Ejemplo: api/pokemon?page=1&tamanio=100
+app.get('/empleados/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('req.query.page: ', req.query.page);
+    const empleadosFounded = songs.find(element => element.id === parseInt(id));
+    res.json(empleadosFounded);
+});
+
+// POST: Crear un nuevo empleado
+app.post('/empleados', (req, res) => {
+    // console.log('req: ', req);
+    // console.log('req.body: ', req.body);
+    const newEmpleado = { ...req.body, id: empleados.length + 1 };
+    songs.push(newEmpleado);
+    res.status(201).json({ message: 'Empleado agregado correctamente', empleado: newEmpleado });
+});
+
+// PATCH: Modificaciones de algunas propiedades del objeto
+app.patch('/empleados/:id', (req, res) => {
+    const id = req.params.id;
+    const newBody = req.body;
+    const positionFounded = empleados.findIndex(element => element.id === parseInt(id));
+    // ... spread operator, se utiliza para hacer destructuring
+    const newEmpleado = { ...empleados[positionFounded], ...newBody };
+    empleados[positionFounded] = newEmpleado;
+    res.status(200).json({ id });
+});
+
+// PUT: Modificar todas las propiedades de un objeto
+app.put('/empleados/:id', (req, res) => {
+    const id = req.params.id;
+    const newBody = req.body;
+    const foundedPosition = empleados.findIndex(element => element.id === parseInt(id));
+    empleados[foundedPosition] = newBody;
+    res.status(200).json({ id });
+});
+
+// DELETE: Eliminar objeto
+app.delete('/empleados/:id', (req, res) => {
+    const id = req.params.id;
+    const foundedPosition = empleados.findIndex(element => element.id === parseInt(id));
+    empleados.splice(foundedPosition, 1);
+    res.status(200).json({ id });
+});
+
+app.listen(port, () => {
+    console.log(`servidor iniciado... en el puerto ${port}`.rainbow);
+});
